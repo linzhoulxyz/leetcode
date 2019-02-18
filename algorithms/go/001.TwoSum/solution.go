898b3df350d7de22e61ca81b3f8ed3e13f81719f
linzhoulxyz <linzhoulxyz@gmail.com>
2019-02-18 22:01:32 +0800

    001优化解答

--- algorithms/go/001.TwoSum/solution.go
+++ algorithms/go/001.TwoSum/solution.go
@@ -1,22 +1,37 @@
-package TwoSum
+package twosum
 
 func twoSum(nums []int, target int) []int {
-    var i, j int
-    var found = false
-    var result = []int{}
-    for i = 0; i < len(nums) - 1; i++ {
-        if found {
-            break
-        }
-        
-        for j = i + 1; j < len(nums); j++ {
-            if nums[i] + nums[j] == target {
-                found = true
-                result = append(result, i, j)
-                break;
-            }
-        }
-    }
-    
-    return result
+	var i, j int
+	var found = false
+	var result = []int{}
+	for i = 0; i < len(nums)-1; i++ {
+		if found {
+			break
+		}
+
+		for j = i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				found = true
+				result = append(result, i, j)
+				break
+			}
+		}
+	}
+
+	return result
+}
+
+// 优化版本
+func twoSum2(nums []int, target int) []int {
+	maps := map[int]int{}
+	for i, a := range nums {
+		b := target - a
+		if j, ok := maps[b]; ok {
+			return []int{j, i}
+		}
+
+		maps[a] = i
+	}
+
+	return nil
 }
--- algorithms/go/001.TwoSum/solution_test.go
+++ algorithms/go/001.TwoSum/solution_test.go
@@ -1,16 +1,28 @@
-package TwoSum
+package twosum
 
 import (
-	"testing"
 	"reflect"
+	"testing"
 )
 
 func TestTwoSum(t *testing.T) {
-	input := []int{2,7,11,15}
+	input := []int{2, 7, 11, 15}
 	target := 9
 	want := []int{0, 1}
 
 	got := twoSum(input, target)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("Excepted %q, got %q", want, got)
+	}
+}
+
+func TestTwoSum2(t *testing.T) {
+	input := []int{2, 7, 11, 15}
+	target := 9
+	want := []int{0, 1}
+
+	got := twoSum2(input, target)
 
 	if !reflect.DeepEqual(got, want) {
 		t.Fatalf("Excepted %q, got %q", want, got)
